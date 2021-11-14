b239a3c99c84e0dd118deff09be9aab51cf576b8
kbetker <kevin@betker.org>
2021-11-13 21:10:31 -0600

    adding some tools to the map

--- frontend/src/components/Locations/Locations.js
+++ frontend/src/components/Locations/Locations.js
@@ -1,8 +1,9 @@
-import React, { useEffect, useRef, useState } from "react"
+import React, { useEffect, useState } from "react"
 import { useParams } from "react-router"
 import { useDispatch } from "react-redux"
 import { fetchMapData } from "../../store/map"
 import Map from "../Map"
+import MapNav from "../MapNav"
 import "./Location.css"
 
 // import PinchZoomPan from "react-image-zoom-pan";
@@ -11,13 +12,12 @@ import { TransformWrapper, TransformComponent } from "react-zoom-pan-pinch";
 function Locations() {
 
     const [windowWidth, setWindowWidth] = useState(window.innerWidth)
-    // const transformWrapper = useRef()
     const [isLoaded, setIsLoaded] = useState(false)
     const dispatch = useDispatch()
     const { id } = useParams()
 
     useEffect(() => {
-        dispatch(fetchMapData(id)).then(()=> setIsLoaded(true))
+        dispatch(fetchMapData(id)).then(() => setIsLoaded(true))
 
         window.addEventListener("resize", (e) => {
             setWindowWidth(window.innerWidth)
@@ -29,14 +29,27 @@ function Locations() {
         <TransformWrapper limitToBounds={false} initialScale={0.27} maxScale={1.5} minScale={0.25} >
             {({ zoomIn, zoomOut, resetTransform, ...rest }) => (
                 <React.Fragment>
+                    <div  className="wut">
                     <div className="tools">
                         <button onClick={() => zoomIn()}>+</button>
                         <button onClick={() => zoomOut()}>-</button>
                         <button onClick={() => resetTransform()}>Reset View</button>
                     </div>
-                    <TransformComponent contentStyle={{ width: `${windowWidth - 30}px`, height: `80vh` }} wrapperStyle={{ backgroundColor: "black", backgroundImage: "url(https://www.otherworldlyincantations.com/wp-content/uploads/Otherworldly-Incantations-Landform-Worldbuilding.jpg)", backgroundSize: "cover" }}>
-                      {isLoaded &&  <Map />}
+
+                    <TransformComponent
+                        contentStyle={{ width: `${windowWidth - 30}px`, height: `88vh` }}
+                        wrapperClass="transformComp"
+                        wrapperStyle={{
+                            backgroundImage: "url(https://www.otherworldlyincantations.com/wp-content/uploads/Otherworldly-Incantations-Landform-Worldbuilding.jpg)",
+                            backgroundSize: "cover"
+                        }}
+                     >
+                        { isLoaded && <Map /> }
                     </TransformComponent>
+
+                    <MapNav />
+
+                    </div>
                 </React.Fragment>
             )}
         </TransformWrapper>
@@ -48,7 +61,3 @@ function Locations() {
 }
 
 export default Locations;
-
-            // <TransformComponent contentStyle={{ width: `${windowWidth - 30}px`, height: `85vh`}} wrapperStyle={{backgroundColor: "black"}}>
-            //     <Map/>
-            // </TransformComponent>
--- frontend/src/components/Navigation/Navigation.js
+++ frontend/src/components/Navigation/Navigation.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { NavLink } from 'react-router-dom';
 import { useSelector } from 'react-redux';
 import ProfileButton from './ProfileButton';
+import "./Navigation.css"
 
 function Navigation({ isLoaded }){
   const sessionUser = useSelector(state => state.session.user);
@@ -14,22 +15,22 @@ function Navigation({ isLoaded }){
   } else {
     sessionLinks = (
       <>
-         <li><NavLink to="/login">Log In</NavLink></li>
-         <li><NavLink to="/signup">Sign Up</NavLink></li>
+         <div className="navElement"><NavLink to="/login">Log In</NavLink></div>
+         <div className="navElement"><NavLink to="/signup">Sign Up</NavLink></div>
       </>
     );
   }
 
   return (
-    <ul>
+    <div className="navBar">
 
-      <li><NavLink exact to="/">Home</NavLink></li>
-      <li> <NavLink exact to="/locations/1">Location 1</NavLink> </li>
-      <li> <NavLink exact to="/locations/2">Location 2</NavLink> </li>
+      <div className="navElement"><NavLink exact to="/">Home</NavLink></div>
+      <div className="navElement"> <NavLink exact to="/locations/1">Location 1</NavLink> </div>
+      <div className="navElement"> <NavLink exact to="/locations/2">Location 2</NavLink> </div>
       {/* <li> <NavLink exact to="/all/1">All</NavLink> </li> */}
         {isLoaded && sessionLinks}
 
-    </ul>
+    </div>
   );
 }
 
